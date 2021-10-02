18bcd65897c717eab29d011f3706476d2e775b40
suey0128 <syui0128@gmail.com>
2021-10-02 16:07:38 -0700

    finail ver

--- stretchgoal3.js
+++ stretchgoal3.js
@@ -6,20 +6,18 @@ window.addEventListener('DOMContentLoaded', (event) => {
     document.querySelector('#lazyMsgThree').style.display = 'none'
     
     disPlayComment();
-
-
-
+    fetchLocalJson();
 });
 
-// how to get the next date https://flaviocopes.com/how-to-get-tomorrow-date-javascript/#:~:text=getDate()%20%2B%201%20%2C%20you',year%2C%20if%20it's%2031%20December.&text=tomorrow%20is%20now%20a%20Date%20object%20representing%20tomorrow's%20date.
-
-let datePicker = document.querySelector('#date-picker')
-
+let LOCAL_DB = [];
+let SAVE_DB = [];
 
 // set timepicker min and max date
+let datePicker = document.querySelector('#date-picker')
 function setDateMinMax () {
     const today = new Date();
     const maxDate = new Date(today);
+    console.log(today);
     maxDate.setDate(maxDate.getDate()-2);
     const maxDateYear = maxDate.getFullYear(); // number type
     let maxDateMonth = maxDate.getMonth() +1; 
@@ -31,18 +29,6 @@ function setDateMinMax () {
     datePicker.setAttribute('max',`${maxDateYear}-${maxDateMonth}-${maxDateDate}`)
 
     initPage (maxDateYear,maxDateMonth,maxDateDate)
-
-}
-
-const apiKey = 'SutcuqvSUtfX12wwW5bWhHEd9tnMxItrzcSbLvYm'
-
-function initPage (maxDateYear,maxDateMonth,maxDateDate) {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&start_date=${maxDateYear}-${maxDateMonth}-${maxDateDate}`)
-    .then(res => res.json())
-    .then((data) => {
-        displayData (data)
-    })
-    .catch(error => console.error('Error:', error))
 }
 
 
@@ -53,7 +39,8 @@ datePicker.addEventListener('change', (e) =>{
     const endDateYear = endDate.getFullYear(); // number type
     const endDateMonth = endDate.getMonth() +1; 
     const endDateDate = endDate.getDate();
-    getData (endDateYear,endDateMonth,endDateDate) 
+    getData (endDateYear,endDateMonth,endDateDate)
+
 })
 
 function getData (endDateYear,endDateMonth,endDateDate) {
@@ -61,13 +48,58 @@ function getData (endDateYear,endDateMonth,endDateDate) {
     fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&start_date=${datePicker.value}&end_date=${endDateYear}-${endDateMonth}-${endDateDate}`)
     .then(res => res.json())
     .then((data) => {
-        console.log(data)
-        displayData (data)
+        workflow(data);
+        fetchLocalJson();
+        displayLike();
     })
     .catch(error => console.error('Error:', error))
 }
 
-//function display data when after data is fetched
+// fetch from nasa
+const apiKey = 'SutcuqvSUtfX12wwW5bWhHEd9tnMxItrzcSbLvYm'
+
+function initPage (maxDateYear,maxDateMonth,maxDateDate) {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&start_date=${maxDateYear}-${maxDateMonth}-${maxDateDate}`)
+    .then(res => res.json())
+    .then((data) => {
+        workflow(data);
+        displayLike();
+    })
+    .catch(error => console.error('Error:', error))
+}
+
+
+//fetch from local json
+const fetchLocalJson = () => {
+    fetch(`http://localhost:3000/likes`)
+    .then(res => res.json())
+    .then((data) => {
+         data.forEach( (item) => LOCAL_DB.push({"id": item.id,
+                                                "date": item.date,
+                                                "like": item.like}))
+    })
+    .catch(error => console.error('Error:', error))
+}
+
+
+//Can't decide button event listener
+document.querySelector('#random').addEventListener('click', (e) => {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=3`)
+    .then(res => res.json())
+    .then((data) => {
+        workflow(data);
+        fetchLocalJson();
+        displayLike();
+    })
+})
+
+//all the work require to post to local json
+function workflow(data){
+    displayData (data);
+    ListAllToStore(data);
+    postLocalJson();
+}
+
 function displayData (data) {
 
     const photoOneMediaWrapper = document.querySelector('#photoOneMediaWrapper')
@@ -157,130 +189,84 @@ function displayData (data) {
         photoThreeDate.textContent = data[2].date
     }
  
-    displayLike()
-    console.log('displayData')
+    // displayLike()
+    // console.log('displayData')
 }
 
 
-
-const likeBtnClick = (item) => {
-    document.querySelector('.like').addEventListener('click', (e) => {
-        console.log(e.target)
-        // console.log(e.target.parentNode.parentNode.children[3]) => date.date
-        const likeP = e.target.parentNode.children[1]
-        let likeDate = e.target.parentNode.parentNode.children[3].textContent
-        let likeNum;
-
-
-            if (likeDate === item.date) {
-                let likeNumInDataBase = parseInt(item.like)
-                console.log(likeNumInDataBase)
-                likeNum = likeNumInDataBase +1;
-                console.log(likeNum)
-                likeP.textContent = `${likeNum} likes`;
-                console.log(likeP)
-                let itemId = item.id;
-                console.log(itemId)
-                patchDatabase(itemId,likeDate,likeNum)
-                console.log('here')
-
-            } 
-
-
-
-
-            })
-   
-
-
-}
-
-
-// //like button event listener (count likes)
-// const likeBtnClick = (item) => {
-//     const likeBtns = document.querySelectorAll('.like');
-//     for (likeBtn of likeBtns) {
-//         likeBtn.textContent = '❤️';
-//         // console.log('dateonpage:',likeBtn.parentNode.nextElementSibling.nextElementSibling)
-       
-//         likeBtn.addEventListener('click', (e) => {
-//             console.log(e.target)
-//             // console.log(e.target.parentNode.parentNode.children[3]) => date.date
-//             const likeP = e.target.parentNode.children[1]
-//             let likeDate = e.target.parentNode.parentNode.children[3].textContent
-//             let likeNum;
-
-//             if (likeDate === item.date) {
-//                 let likeNumInDataBase = parseInt(item.like)
-//                 console.log(likeNumInDataBase)
-//                 likeNum = likeNumInDataBase +1;
-//                 console.log(likeNum)
-//                 likeP.textContent = `${likeNum} likes`;
-//                 console.log(likeP)
-//                 let itemId = item.id;
-//                 console.log(itemId)
-//                 patchDatabase(itemId,likeDate,likeNum)
-//             } else {
-//                 likeNum = 1;
-//                 likeP.textContent = `1 like`;
-//                 console.log(console.log(e.target))
-//                 // postDatabase(likeDate,likeNum)
-//             }
-
-//         })
-//     }
-// }
-
-
-//comment form event listener
-// document.querySelector('#comments').innerHTML = '';
-document.querySelector('#commentForm').addEventListener('submit',(e) => {
-    e.preventDefault();
-    let commentLi = document.createElement('li')
-    commentLi.textContent =  document.querySelector('#commentBox').value;
-    commentLi.className = "comment"
-    document.querySelector('#comments').append(commentLi);
-    alert('★ The universe thanks you for sharing ☆');
-
-    createComment(commentLi)
-})
-
-
-//Can't decide button event listener
-document.querySelector('#random').addEventListener('click', (e) => {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=3`)
-    .then(res => res.json())
-    .then((data) => {
-        console.log(data)
-        displayData (data)
-    })
-})
-
-
-
-//function to create new item for the local database
-const postDatabase = (likeDate,likeNum) => {
-    fetch(`http://localhost:3000/item`,{
-        method:'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            date: likeDate,
-            like: likeNum
+const ListAllToStore = (data) => {
+    let flag = false;
+    data.forEach((elem) => {
+        LOCAL_DB.forEach((item) => {
+            if (item.date === elem.date){
+                flag = true;
+            }
         })
+        if (!flag){
+            SAVE_DB.push(elem.date);
+        }
+        else{
+            flag = false;
+        }
     })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('POST Error: ', error))
+    
+}
+
+const postLocalJson = () => {
+    // console.log(SAVE_DB);
+    SAVE_DB.forEach((item) => {
+            fetch(`http://localhost:3000/likes`,{
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                date: item,
+                like: 0
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            SAVE_DB = [];
+        })
+        .catch(error => console.error('POST Error: ', error))   
+    })
 }
 
 
+document.querySelector('.like1').addEventListener('click', (e) => {
+    // console.log(e.target.parentNode.parentNode.children[3]) => date.date
+    getlikes(e);
+})
+
+document.querySelector('.like2').addEventListener('click', (e) => {
+    // console.log(e.target.parentNode.parentNode.children[3]) => date.date
+    getlikes(e);
+
+})
+
+document.querySelector('.like3').addEventListener('click', (e) => {
+    // console.log(e.target.parentNode.parentNode.children[3]) => date.date
+    getlikes(e);
+
+})
+function getlikes(e){
+    const likeP = e.target.parentNode.children[1]
+    let likeDate = e.target.parentNode.parentNode.children[3].textContent
+    let likeNum;
+    console.log(LOCAL_DB);
+    LOCAL_DB.forEach((item)=> {
+        if(item.date === likeDate){
+            patchDatabase(item.id, item.date, ++item.like)
+        }
+    })
+    displayLike();
+}
 
 
-//PATCH to the local database
 function patchDatabase(itemId,likeDate,likeNum){
-    fetch(`http://localhost:3000/item/${itemId}`,{
+    console.log(itemId,likeDate,likeNum)
+    fetch(`http://localhost:3000/likes/${itemId}`,{
         method:'PATCH',
         headers: {
             'Content-Type': 'application/json'
@@ -295,20 +281,10 @@ function patchDatabase(itemId,likeDate,likeNum){
     .catch(error => console.error('POST Error: ', error))
 }
 
-//del item on database base on id
-function deleteData(keyName,id){
-    fetch(`http://localhost:3000/${keyName}/${id}`,{
-        method:'DELETE',
-        headers:{
-            'Content-Type':'application/json'
-        }
-    })
-    .catch(error => console.error('Error:', error))
-}
 
 //Fetch likes
 const displayLike = () => {
-    fetch(`http://localhost:3000/item`)
+    fetch(`http://localhost:3000/likes`)
     .then(res => res.json())
     .then((data) => {
         data.forEach(item => {
@@ -322,14 +298,11 @@ const displayLike = () => {
                 document.querySelector('#photoThreeDate').parentNode.children[1].children[1].textContent = `${item.like} like`;
             } else {
             }
-            likeBtnClick(item)
             // console.log('item')
         })
     })
 
 }
-
-
 
 
 //display comment when it first load
@@ -349,12 +322,48 @@ const disPlayComment = () => {
 }
 
 
+//comment form event listener
+// document.querySelector('#comments').innerHTML = '';
+document.querySelector('#commentForm').addEventListener('submit',(e) => {
+    e.preventDefault();
+    let commentLi = document.createElement('li')
+    commentLi.textContent =  document.querySelector('#commentBox').value;
+    commentLi.className = "comment"
+    document.querySelector('#comments').append(commentLi);
+    alert('★ The universe thanks you for sharing ☆');
+
+    postDatabase(commentLi)
+})
+
+
+
+//function to create new item for the local database
+const postDatabase = (commentLi) => {
+    fetch(`http://localhost:3000/comments`,{
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            comment:commentLi.textContent
+        })
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('POST Error: ', error))
+}
+
+document.querySelectorAll('.photoMediaWrapper').forEach((mediaWrapper) => {
+    mediaWrapper.addEventListener('click', (e) => {
+        let large = e.target.cloneNode(true);
+        large.className='';
+        large.id ='';
+        console.log(large)
+
+
+    })
 
 
 
 
-
-
-
-
-// deleteData('comments','FtvJQRF')
+})
